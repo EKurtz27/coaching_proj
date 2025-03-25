882da49249689a4d4b1544286a03f77969e07b02
EvanK <evan_kurtz@outlook.com>
2025-03-25 10:38:31 -0400

    built relation between teams and wikipedia page titles

--- coaching_proj.py
+++ coaching_proj.py
@@ -2,10 +2,30 @@ from bs4 import BeautifulSoup
 import requests
 import wikipedia as wp
 import pandas as pd
+import pprint
 
 # Fetch the Wikipedia page content
 page = wp.page("List of NCAA Division I FBS football programs")
 read_page = pd.read_html(page.html())
 cfb_geninfo = read_page[0]
 schools = cfb_geninfo['School'].tolist()
-print(schools)
+team_sites = []
+for school in schools:
+    search_result = wp.search(school + " football", results=1)
+    team_tuple = (school, search_result[0])
+    team_sites.append(team_tuple)
+#pprint.pprint(team_sites)
+#misidentified schools: Charlotte: Charlotte FC, Houston: Houston Texans, Miami(FL): 2001 Miami Hurricanes football team
+#misid cont: Ohio: Ohio State Buckeyes football, Washington: Washington Commanders
+for i, item in enumerate(team_sites):
+    if item[0] == "Charlotte":
+        team_sites[i] = ("Charlotte 49ers", "Charlotte 49ers football")
+    if item[0] == "Houston":
+        team_sites[i] = ("Houston Cougars", "Houston Cougars football")
+    if item[0] == "Miami (FL)":
+        team_sites[i] = ("Miami Hurricanes", "Miami Hurricanes football")
+    if item[0] == "Ohio":
+        team_sites[i] = ("Ohio Bobcats", "Ohio Bobcats football")
+    if item[0] == "Washington":
+        team_sites[i] = ("Washington Huskies", "Washington Huskies football")
+pprint.pprint(team_sites)
