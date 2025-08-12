0d5ce35e7d450cb9df75eef9539010d64ef2080c
EvanK <evan_kurtz@outlook.com>
2025-08-12 13:24:19 -0700

    Updated BFS and cleaning functions

--- coach_lca_alg.py
+++ coach_lca_alg.py
@@ -1,6 +1,8 @@
 import pickle
 import networkx as nx
+import pandas as pd
 from basic_graph_generation import *
+import ast
 
 # input_file_name = input("Please enter the path to the CSV file you wish to read: ").strip()
 # with open(f"{input_file_name}", "r") as coach_jobs_csv:
@@ -48,24 +50,56 @@ def remove_future_edges(graph:nx.MultiDiGraph, last_valid_year: str) -> nx.Multi
         graph.remove_edge(u, v, k)
     return graph
 
-def remove_edges_of_current_staff(graph: nx.MultiDiGraph, coach: str,  year: int) -> nx.MultiDiGraph: # Fix to collect before removal. Add keys
+def is_promotion(connection_years: set, edge_data) -> bool:
+    if not connection_years:
+        return False
+    edge_final_year = max(edge_data.get("years_of_connection"))
+    edge_first_year = min(edge_data.get("years_of_connection"))
+    tracked_min_year = min(connection_years)
+    tracked_max_year = max(connection_years)
+    if (edge_final_year + 1 == tracked_min_year or
+        edge_first_year - 1 == tracked_max_year):
+        return True
+    else:
+        return False
+
+def remove_edges_of_last_job(graph: nx.MultiDiGraph, team: str,  coach: str, last_valid_year: int) -> nx.MultiDiGraph: # Fix to collect before removal. Add keys
+    hc_coach_connection_years = set()
+    for u, v, k, edge_data in graph.edges(data=True, keys=True):
+        if (1 in edge_data.get("encoded_connection") and
+            (v == coach or u == coach) and edge_data.get("team_of_connection") == team and 
+            (last_valid_year in edge_data.get("years_of_connection") or is_promotion(hc_coach_connection_years, edge_data))
+            ): 
+            # Issue: some people get promoted, creating a seperate edge (last valid year not in new edge) but should still be removed.
+            # Solution? If lowest number in coach_connection_years is 1 less than a year in this edge, this represents a promotion
+            hc_coach_edge_data = edge_data
+            hc_coach_connection_years.update(edge_data.get("years_of_connection"))
+    
+    if hc_coach_connection_years == None:
+        print("HC to coach connection not found!")
+        print(coach)
+        import sys
+        sys.exit()
+
     edges_to_remove = []
     for u, v, k, edge_data in graph.edges(data=True, keys= True):
-        years = edge_data.get("years_of_connection")
-        team = edge_data.get("team_of_connection")
-        if test_year in years and team == find_team_of_coach_year(G, "Dan Lanning", 2024):
+        edge_years = edge_data.get("years_of_connection")
+        edge_team = edge_data.get("team_of_connection")
+        if any(y in edge_years for y in hc_coach_connection_years) and edge_team == team: # Make sure paths aren't drawn through edges formed while coach was already under current head coach
             edges_to_remove.append((u, v, k))
     for u, v, k in edges_to_remove:
         graph.remove_edge(u, v, k)
     return graph
 
 
-def clean_graph(graph: nx.MultiDiGraph, coach: str, last_valid_year: int) -> nx.MultiDiGraph:
+def clean_graph(graph: nx.MultiDiGraph, team: str, coach: str, last_valid_year: int) -> nx.MultiDiGraph:
     # print(G.number_of_edges())
-    keep_only_mentorship_true_edges(graph)
-    remove_future_edges(graph, last_valid_year)
-    remove_edges_of_current_staff(graph, coach, last_valid_year)
+    new_graph = graph.copy()
+    keep_only_mentorship_true_edges(new_graph)
+    remove_future_edges(new_graph, last_valid_year)
+    remove_edges_of_last_job(new_graph, team, coach, last_valid_year)
     # print(G.number_of_edges())
+    return new_graph
 
 def chronologic_sentsitive_bfs_shortest_path_length(graph: nx.MultiDiGraph, start_node: str, end_node: str) -> int:
     explored = set()
@@ -99,6 +133,7 @@ def chronologic_sentsitive_bfs_shortest_path_length(graph: nx.MultiDiGraph, star
                         new_path.append( (coach_node, edge_key, edge_data) )
                         if coach_node == end_node:
                             return new_path
+                        queue.append(new_path)
         explored.update(newly_explored)  
     return None                
 
@@ -130,24 +165,72 @@ def find_lowest_common_ancestor(cleaned_graph: nx.MultiDiGraph, coach1: str, coa
             min_dist = total_dist
             closest = ancestor
 
-    # Current status: works (kinda)! Issue is that connections are drawn with no respect for year
-    # Example: **Dan Lanning -> Brad Sherrod -> Justin Burke <- **Will Stein
-    # Brad Sherrod did coach under Justin Burke in 2022/2023, but that is **after** Dan Lanning coached under Brad Sherrod in 2014
-    # This doesn't accurately represent the passing down of coaching theory
-    # Possible solution: custom BFS search that takes year into account after finding shared descendents
-    print(f"c1: {min_c1_path}")
-    print(f"c2: {min_c2_path}")
+    # print(f"{coach1}: {min_c1_path}")
+    # print(f"{coach2}: {min_c2_path}")
     return closest, min_dist
+
+def list_distance_scores(graph: nx.MultiDiGraph, team: str, year: int) -> list:
+    relevant_coaches = set()
+    head_coach = None
+    for u, v, k, edge_data in graph.edges(data=True, keys= True):
+        edge_years = edge_data.get("years_of_connection")
+        edge_team = edge_data.get("team_of_connection")
+        if year in edge_years and edge_team == team:
+            relevant_coaches.add(u)
+            relevant_coaches.add(v)
+            if edge_data.get("source_position", "") == "Head Coach":
+                head_coach = u
+            if edge_data.get("target_position", "") == "Head Coach":
+                head_coach = v
+    if head_coach == None:
+        print(f"{team} does not have a head coach")
+        return None
+    relevant_coaches.remove(head_coach)
+    print(relevant_coaches)
+    print(head_coach)
+    distance_list = []
+    for coach2 in relevant_coaches:
+        indiv_coach_graph = clean_graph(graph, team, coach2, year) # Head coach already identified, integrate?
+        distance_list.append({
+            f"{head_coach} -> {coach2}": find_lowest_common_ancestor(indiv_coach_graph, head_coach, coach2)
+            }) 
+    return distance_list
 
 # first_edge = next(iter(G.edges(data=True)))
 # print(first_edge)
 
-clean_graph(G, "Dan Lanning", 2025)
-closest, min_dist = find_lowest_common_ancestor(G, "Dan Lanning", "Will Stein")
-print(closest)
-print(min_dist) 
+# clean_graph(G, "Dan Lanning", 2025)
+# closest, min_dist = find_lowest_common_ancestor(G, "Dan Lanning", "Will Stein")
+# print(closest)
+# print(min_dist) 
 
 # Lanning_preds = G.pred["Dan Lanning"]
 # for coach, edge_dict in Lanning_preds.items():
 #     for edge_key, edge_data in edge_dict.items():
 #         print(f"{coach}: {edge_data.get("team_of_connection")}") #.get does work at this stage
+
+if __name__ == "__main__":
+    with open("data/clean_sorted_coach_jobs.csv", "r") as f:
+        coach_jobs = pd.read_csv(f)
+
+    # Convert the column to lists
+    coach_jobs['Seasons at Position'] = coach_jobs['Seasons at Position'].apply(ast.literal_eval)
+
+    # Now you can filter as intended
+    teams_for_year_df = coach_jobs[coach_jobs['Seasons at Position'].apply(lambda x: 2025 in x)]
+    teams_for_year = teams_for_year_df["Team"].unique()
+
+    distances_2025 = []
+
+    for team in teams_for_year:
+        team_dist_list = list_distance_scores(G, team, 2025)
+        distances_2025.append(
+            {
+                "Team": team,
+                "Distance List": team_dist_list
+            }
+        )
+
+    distances_2025_df = pd.DataFrame(distances_2025)
+
+    distances_2025_df.to_csv("test.csv", index=False)
